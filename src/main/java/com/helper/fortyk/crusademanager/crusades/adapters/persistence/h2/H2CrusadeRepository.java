0c4241623c59bd10bc067381592c6f614c47b644
tinqiu <vincent.brunet.vb@gmail.com>
2021-08-03 15:32:28 -0400

    javadoc changes

--- src/main/java/com/helper/fortyk/crusademanager/crusades/adapters/persistence/h2/H2CrusadeRepository.java
+++ src/main/java/com/helper/fortyk/crusademanager/crusades/adapters/persistence/h2/H2CrusadeRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Collections;
 
 /**
- * H2 implementation of the {@link CrusadeRepositoryPort} implementation.
+ * H2 implementation of the {@link CrusadeRepositoryPort} interface.
  */
 @Repository
 @RequiredArgsConstructor
--- src/main/java/com/helper/fortyk/crusademanager/crusades/domain/ports/CrusadeRepositoryPort.java
+++ src/main/java/com/helper/fortyk/crusademanager/crusades/domain/ports/CrusadeRepositoryPort.java
@@ -2,14 +2,10 @@ package com.helper.fortyk.crusademanager.crusades.domain.ports;
 
 import com.helper.fortyk.crusademanager.crusades.domain.model.crusade.Crusade;
 import com.helper.fortyk.crusademanager.crusades.domain.model.crusade.CrusadeId;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Spring Framework handled {@link JpaRepository}.
- * <p>
  * Represents the outbound Persistence port of the {@link Crusade} domain entity.
  */
-
 public interface CrusadeRepositoryPort {
     Crusade getById(CrusadeId id);
 
